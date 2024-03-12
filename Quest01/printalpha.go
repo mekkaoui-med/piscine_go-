08f7bead23c7efe3d06c51f3edb2a772719b5631
MedMekkaoui <mekkaouimed20@gmail.com>
2024-03-12 02:47:07 +0000

    piscine_go

--- Quest01/printalpha.go
+++ Quest01/printalpha.go
@@ -2,8 +2,22 @@ package main
 
 import "fmt"
 
-func main(){
+func PrintAlpha_Ascending(){
 	for i:='a';i<='z';i++{
 		fmt.Printf("%c",i)
 	}
+	fmt.Println()
+}
+
+func PrintAlpha_Descending(){
+	for i:='z';i>='a';i--{
+		fmt.Printf("%c",i)
+	}
+	fmt.Println()
+}
+
+func main(){
+	PrintAlpha_Ascending()
+	PrintAlpha_Descending()
+	
 }
