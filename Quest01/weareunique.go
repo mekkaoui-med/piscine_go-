f74f58bd9379a381c23420f9c43fb5834da3e830
MedMekkaoui <mekkaouimed20@gmail.com>
2024-03-14 23:17:11 +0000

    piscine_go

---
+++ Quest01/weareunique.go
@@ -0,0 +1,23 @@
+package main
+
+
+func Weareunique(str1 , str2 string) int {
+	if str1 == "" && str2=="" {
+		return -1
+		
+	}
+	str := str1 +str2 
+	count := 0
+	fot _, s:= range str {
+		unique :=true
+		for j,c:=range str {
+			if s == c && i !=j{
+				unique = false
+			}
+			if unique {
+				count ++ 
+			}
+		}
+		return count 
+	}
+}
